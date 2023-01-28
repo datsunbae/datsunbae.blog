6af7d9f34163217866a5e8c0d55202a51aa2d3a1
Nguyen Van Dat <datsunbae@gmail.com>
2023-01-29 00:04:42 +0700

    Prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Welcome to Datsunbae Blog!
-    </h1>
-  )
+    <main className="w-screen h-screen overflow-auto flex flex-col items-center bg-zinc-800 text-neutral-300">
+      <title>Home Page</title>
+      <section>
+        <div className="mt-3 text-center">
+          <h1 className="text-[3rem]">Welcome to Datsunbae Blog</h1>
+          <p>I share Engineer and some thought in Product and Life 😴</p>
+        </div>
+      </section>
+    </main>
+  );
 }
